865846bfeccc2b0a3871b793bc3b1da372f801d7
nykoh2001 <nykoh2001@gmail.com>
2024-02-07 03:52:57 +0900

    [chore] #5 안쓰는 패키지 임포트 제거

--- src/main/java/org/harang/server/service/MatchingService.java
+++ src/main/java/org/harang/server/service/MatchingService.java
@@ -5,13 +5,10 @@ import lombok.RequiredArgsConstructor;
 import org.harang.server.domain.Member;
 import org.harang.server.domain.Post;
 import org.harang.server.dto.request.MatchingRequest;
-import org.harang.server.dto.type.ErrorMessage;
-import org.harang.server.exception.CustomException;
 import org.harang.server.repository.MatchingRepository;
 import org.harang.server.repository.MemberRepository;
 import org.harang.server.repository.PostRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 @RequiredArgsConstructor
