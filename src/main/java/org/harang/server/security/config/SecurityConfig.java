ae6adf0c6215153dba0e7fcac6facca867549aad
nykoh2001 <nykoh2001@gmail.com>
2024-02-09 02:14:46 +0900

    [feat] #6 add jwt auth filter

--- src/main/java/org/harang/server/security/config/SecurityConfig.java
+++ src/main/java/org/harang/server/security/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package org.harang.server.security.config;
 
+import lombok.RequiredArgsConstructor;
+import org.harang.server.security.filter.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,10 +9,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -25,6 +30,7 @@ public class SecurityConfig {
                                 // TODO: 이후에 구현되는 로그인 api 엔드포인트랑 동일한지 확인
                                 .requestMatchers("/v1/login").permitAll()
                                 .anyRequest().authenticated())
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
