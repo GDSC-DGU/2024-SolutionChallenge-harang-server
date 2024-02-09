92cd5d79c764372e4503ab73fe98a8430ca98234
nykoh2001 <nykoh2001@gmail.com>
2024-02-09 21:07:18 +0900

    [feat] custom jwt auth. entry point

---
+++ src/main/java/org/harang/server/security/filter/CustomJwtAuthenticationEntryPoint.java
@@ -0,0 +1,31 @@
+package org.harang.server.security.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.harang.server.dto.common.ApiResponse;
+import org.harang.server.dto.type.ErrorMessage;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomJwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+
+    }
+
+    private void setResponse(HttpServletResponse response) throws IOException {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getWriter().println(objectMapper.writeValueAsString(ApiResponse.fail(ErrorMessage.UNAUTHORIZED)));
+    }
+}
